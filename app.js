71e6de91b7ac18fc0b7ef01f4d9c9d58ca2c3665
Chai Phonbopit <phonbopit@gmail.com>
2022-07-14 23:57:40 +0700

    add multiple files

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ const multer = require('multer')
 
 const app = express()
 
-const upload = multer({ dest: 'uploads' })
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, 'uploads')
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname)
+  },
+})
 
-app.get('/', (rea, res) => {
+const upload = multer({ storage, limits: { fileSize: 1024 * 1024 } })
+
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/upload', upload.single('photo'), (req, res) => {
   res.send(req.file)
+})
+
+app.post('/images', upload.array('images'), (req, res) => {
+  res.send(req.files)
 })
 
 app.listen(9999, () => console.log('Running on port 9999'))
